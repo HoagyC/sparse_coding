ebf56d54464b6a8bf46ee9127c477f3156fc63bc
HoagyC <hoagycunningham@gmail.com>
2023-06-09 14:29:19 +0100

    Add helper for copying results folders from vast.ai.

--- utils.py
+++ utils.py
@@ -21,15 +21,18 @@ def copy_models():
     command = f"scp -P {VAST_PORT} -r models {dest_addr}:{SSH_DIRECTORY}/models"
     subprocess.call(command, shell=True)
 
-if __name__ == "__main__":
-    if sys.argv[1] == "sync":
-        sync()
-    elif sys.argv[1] == "models":
-        copy_models()
-
+def copy_recent():
+    """ Get the most recent outputs folder in the remote host and copy across to same place in local directory."""
+    # get the most recent folder
+    command = f"ssh -p {VAST_PORT} {dest_addr} \"ls -td {SSH_DIRECTORY}/outputs/* | head -1\""
+    output = subprocess.check_output(command, shell=True)
+    output = output.decode("utf-8").strip()
+    # copy across
+    command = f"scp -P {VAST_PORT} -r {dest_addr}:{output} outputs"
+    subprocess.call(command, shell=True)
 
 class dotdict(dict):
-    # if no dict given, create empty dict
+    """ Dictionary that can be accessed with dot notation."""
     def __init__(self, d: Optional[dict] = None):
         if d is None:
             d = {}
@@ -47,3 +50,11 @@ class dotdict(dict):
     def __delattr__(self, name):
         del self[name]
     
+
+if __name__ == "__main__":
+    if sys.argv[1] == "sync":
+        sync()
+    elif sys.argv[1] == "models":
+        copy_models()
+    elif sys.argv[1] == "recent":
+        copy_recent()
