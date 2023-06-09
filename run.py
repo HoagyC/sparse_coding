f9701cd5a69794bcb66366c34749e800b2e03432
HoagyC <hoagycunningham@gmail.com>
2023-06-09 14:30:50 +0100

    Raise epsilon for Adam in toy models to work with float16.

--- run.py
+++ run.py
@@ -448,7 +448,7 @@ def run_single_go(cfg: dotdict, data_generator: Optional[RandomDatasetGenerator]
 
     ground_truth_features = data_generator.feats
     # Train the model
-    optimizer = optim.Adam(auto_encoder.parameters(), lr=cfg.learning_rate)
+    optimizer = optim.Adam(auto_encoder.parameters(), lr=cfg.learning_rate, eps=1e-4)
     
     # Hold a running average of the reconstruction loss
     running_recon_loss = 0.0
