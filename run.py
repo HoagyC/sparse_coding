f9af21a9ec4b427bc8b69d0e71a445bae9dc69ab
HoagyC <hoagycunningham@gmail.com>
2023-06-09 16:11:18 +0100

    Add top_k activations per feature.

--- run.py
+++ run.py
@@ -15,6 +15,7 @@ from baukit import Trace
 from datasets import Dataset, DatasetDict, load_dataset
 from einops import rearrange
 from matplotlib import pyplot as plt
+import pandas as pd
 import numpy as np
 import torch
 import torch.nn as nn
@@ -704,16 +705,62 @@ def run_single_go_with_real_data(cfg, dataset_folder: str):
     return auto_encoder, n_dead_neurons, running_recon_loss
 
 
-def get_top_k_activations_from_dataset(cfg, feature_dict: torch.Tensor,  k: int = 100):
+def get_top_k_activations_from_dataset(cfg, model: HookedTransformer, feature_dict: torch.Tensor,  k: int = 100, use_baukit: bool = False, max_sentences: int = 10000):
     """
     Takes a dict of features and returns the top k activations for each feature in pile10k
     """
     sentence_dataset = load_dataset(cfg.dataset_name)
+    sentence_dataset = sentence_dataset["train"]
+    if max_sentences is not None and max_sentences < len(sentence_dataset):
+        sentence_dataset = sentence_dataset.select(range(max_sentences))
 
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    tensor_name = make_tensor_name(cfg)
+    # make list of sentence, tokenization pairs
 
+    all_internals = []
+    print(f"Computing internals for all {len(sentence_dataset)} sentences")
 
+    # Make dataframe with columns for each feature, and rows for each sentence fragment
+    # each row should also have the full sentence, the current tokens and the previous tokens
 
-def make_activation_dataset(cfg, sentence_dataset: DataLoader, model: HookedTransformer, tensor_name: str, dataset_folder: str, baukit: bool = False):
+    sentence_fragment_dicts = []
+    for sentence in tqdm(sentence_dataset):
+        tokens = tokenizer(sentence["text"], return_tensors="pt")["input_ids"].to(cfg.device)[:, :cfg.max_length]
+        if use_baukit:
+            with Trace(model, tensor_name) as ret:
+                _ = model(tokens)
+                mlp_activation_data = ret.output
+                mlp_activation_data = rearrange(mlp_activation_data, 'b s n -> (b s) n').to(cfg.device)
+                mlp_activation_data = nn.functional.gelu(mlp_activation_data)
+        else:       
+            _, cache = model.run_with_cache(tokens)
+            mlp_activation_data = cache[tensor_name].to(cfg.device) # NOTE: could do all layers at once, but currently just doing 1 layer
+            mlp_activation_data = rearrange(mlp_activation_data, 'b s n -> (b s) n')
+
+        # Project the activations into the feature space
+        feature_activation_data = torch.matmul(mlp_activation_data, feature_dict.to(torch.float32))
+
+        full_sentence = tokenizer.decode(tokens[0, 1:])
+        for i in range(1, tokens.shape[1]):
+            partial_str_dict: Dict[str, Any] = {}
+            partial_str_dict["pre_tokens"] = tokens[0, 1 : i + 1].tolist()
+            partial_str_dict["full_tokens"] = tokens[0, 1:].tolist()
+            partial_str_dict["sentence"] = full_sentence
+            partial_sentence = tokenizer.decode(tokens[0, 1 : i + 1])
+            partial_str_dict["context"] = partial_sentence
+        
+            for j in range(feature_dict.shape[1]):
+                partial_str_dict[f"feature_{j}"] = feature_activation_data[i, j].item()
+            # add a row into the dataframe for each sentence fragment
+            sentence_fragment_dicts.append(partial_str_dict)
+    
+    df = pd.DataFrame(sentence_fragment_dicts)
+    breakpoint()
+    return df
+
+            
+def make_activation_dataset(cfg, sentence_dataset: DataLoader, model: HookedTransformer, tensor_name: str, dataset_folder: str, baukit: bool = False) -> pd.DataFrame:
     print(f"Running model and saving activations to {dataset_folder}")
     with torch.no_grad():
         chunk_size =  2 * (2 ** 30) # 2GB
@@ -748,6 +795,18 @@ def make_activation_dataset(cfg, sentence_dataset: DataLoader, model: HookedTran
                 if n_saved_chunks == cfg.n_chunks:
                     break
 
+def make_tensor_name(cfg):
+    if cfg.model_name in ["gpt2", "EleutherAI/pythia-70m-deduped"]:
+        tensor_name = f"blocks.{cfg.layer}.mlp.hook_post"
+        cfg.mlp_width = 3072
+    elif cfg.model_name == "nanoGPT":
+        tensor_name = f"transformer.h.{cfg.layer}.mlp.c_fc"
+        cfg.mlp_width = 128
+    else:
+        raise NotImplementedError(f"Model {cfg.model_name} not supported")
+
+    return tensor_name
+
 def run_real_data_model(cfg):
     # cfg.model_name = "EleutherAI/pythia-70m-deduped"
     if cfg.model_name in ["gpt2", "EleutherAI/pythia-70m-deduped"]:
@@ -772,6 +831,9 @@ def run_real_data_model(cfg):
         print("Using default tokenizer from gpt2")
         tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
+    feature_dict = pickle.load(open("outputs/tinybatch_sweep/auto_encoders.pkl", "rb"))[0][0].decoder.weight.detach().cpu()
+    get_top_k_activations_from_dataset(cfg, model, feature_dict, k=10, use_baukit=use_baukit, max_sentences=100)
+
     sentence_dataset, bits_per_byte = chunk_and_tokenize(sentence_dataset, tokenizer, max_length=cfg.max_length)
     sentence_dataset = DataLoader(sentence_dataset, batch_size=cfg.model_batch_size, shuffle=True)
 
@@ -780,14 +842,7 @@ def run_real_data_model(cfg):
     dataset_folder = os.path.join(cfg.datasets_folder, dataset_name)
     os.makedirs(dataset_folder, exist_ok=True)
     if len(os.listdir(dataset_folder)) == 0:
-        if cfg.model_name in ["gpt2", "EleutherAI/pythia-70m-deduped"]:
-            tensor_name = f"blocks.{cfg.layer}.mlp.hook_post"
-            cfg.mlp_width = 3072
-        elif cfg.model_name == "nanoGPT":
-            tensor_name = f"transformer.h.{cfg.layer}.mlp.c_fc"
-            cfg.mlp_width = 128
-        else:
-            raise NotImplementedError(f"Model {cfg.model_name} not supported")
+        tensor_name = make_tensor_name(cfg)
         make_activation_dataset(cfg, sentence_dataset, model, tensor_name, dataset_folder, use_baukit)
     else:
         print(f"Activations in {dataset_folder} already exist, loading them")
