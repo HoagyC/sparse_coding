a1735df628f9729a88400c493fc185c756c0210e
HoagyC <hoagycunningham@gmail.com>
2023-06-23 12:23:48 +0100

    Add upload to AWS util.

--- utils.py
+++ utils.py
@@ -1,13 +1,20 @@
+import json
+import os
 import subprocess
 import sys
 from typing import Optional
+from pathlib import Path
+
+import boto3
+from botocore.exceptions import NoCredentialsError
 
 VAST_NUM = 4
-VAST_PORT = 35344
+VAST_PORT = 16356
 SSH_DIRECTORY = "sparse_coding"
 dest_addr = f"root@ssh{VAST_NUM}.vast.ai"
 SSH_PYTHON = "/opt/conda/bin/python"
 
+BUCKET_NAME = "sparse-coding"
 
 def sync():
     """Sync the local directory with the remote host."""
@@ -19,7 +26,9 @@ def copy_models():
     """Copy the models from local directory to the remote host."""
     command = f"scp -P {VAST_PORT} -r models {dest_addr}:{SSH_DIRECTORY}/models"
     subprocess.call(command, shell=True)
-
+    # also copying across a few other files
+    command = f"scp -P {VAST_PORT} -r outputs/thinrun/autoencoders_cpu.pkl {dest_addr}:{SSH_DIRECTORY}"
+    subprocess.call(command, shell=True)
 
 def copy_secrets():
     """Copy the secrets.json file from local directory to the remote host."""
@@ -29,7 +38,7 @@ def copy_secrets():
 
 def copy_recent():
     """Get the most recent outputs folder in the remote host and copy across to same place in local directory."""
-    # get the most recent folder
+    # get the most recent folders
     command = f'ssh -p {VAST_PORT} {dest_addr} "ls -td {SSH_DIRECTORY}/outputs/* | head -1"'
     output = subprocess.check_output(command, shell=True)
     output = output.decode("utf-8").strip()
@@ -87,6 +96,44 @@ def make_tensor_name(cfg):
         raise NotImplementedError(f"Model {cfg.model_name} not supported")
 
     return tensor_name
+
+def upload_to_aws(local_file_name, s3_file_name: str = "") -> bool:
+    """"
+    Upload a file to an S3 bucket
+    :param local_file_name: File to upload
+    :param s3_file_name: S3 object name. If not specified then local_file_name is used
+    """
+    secrets = json.load(open("secrets.json"))
+
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=secrets["access_key"],
+        aws_secret_access_key=secrets["secret_key"],
+    )
+
+    if not s3_file_name:
+        s3_file_name = local_file_name
+    local_file_path = Path(local_file_name)
+    try:
+        if local_file_path.is_dir():
+            _upload_directory(local_file_name, s3)
+        else:
+            s3.upload_file(str(local_file_name), BUCKET_NAME, str(s3_file_name))
+        print(f"Upload Successful of {local_file_name}")
+        return True
+    except FileNotFoundError:
+        print(f"File {local_file_name} was not found")
+        return False
+    except NoCredentialsError:
+        print("Credentials not available")
+        return False
+    
+def _upload_directory(path, s3_client):
+    for root, dirs, files in os.walk(path):
+        for file_name in files:
+            full_file_name = os.path.join(root, file_name)
+            s3_client.upload_file(str(full_file_name), BUCKET_NAME, str(full_file_name))
+    
 
 if __name__ == "__main__":
     if sys.argv[1] == "sync":
