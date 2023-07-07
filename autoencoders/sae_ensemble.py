fc584ec3e2d91916a8d655c5914c1068933d8f82
Aidan Ewart <aidanprattewart@gmail.com>
2023-07-07 11:41:46 +0100

    parallelized code works apparently!

--- autoencoders/sae_ensemble.py
+++ autoencoders/sae_ensemble.py
@@ -9,19 +9,21 @@ import torchopt
 import copy
 
 class SAE(nn.Module):
-    def __init__(self, activation_size, n_dict_components, l1_coef=0.0):
+    def __init__(self, activation_size, n_dict_components, l1_coef=0.0, device=None):
         super(SAE, self).__init__()
-        self.decoder = nn.Parameter(torch.empty((n_dict_components, activation_size)))
+        if device is None:
+            self.device = torch.device("cpu")
+        else:
+            self.device = device
+
+        self.decoder = nn.Parameter(torch.empty((n_dict_components, activation_size), device=self.device))
         # Initialize the decoder weights orthogonally
         nn.init.orthogonal_(self.decoder)
 
-        self.encoder = nn.Sequential(nn.Linear(activation_size, n_dict_components), nn.ReLU())
+        self.encoder = nn.Sequential(nn.Linear(activation_size, n_dict_components, device=self.device), nn.ReLU())
         self.register_buffer("l1_coef", torch.tensor(l1_coef))
-    
-    def forward(self, x):
-        return self.encoder(x)
 
-    def loss(self, x):
+    def forward(self, x):
         c = self.encoder(x)
         
         # can't use this here due to vmap
@@ -33,5 +35,35 @@ class SAE(nn.Module):
 
         l_reconstruction = F.mse_loss(x_hat, x)
         l_l1 = self.l1_coef * torch.norm(c, 1, dim=1).mean()
+        
+        return l_reconstruction + l_l1
+
+class FunctionalSAE:
+    @staticmethod
+    def init(activation_size, n_dict_components, l1_alpha, device=None):
+        params = {}
+        buffers = {}
+
+        params["decoder"] = torch.empty((n_dict_components, activation_size), device=device)
+        nn.init.orthogonal_(params["decoder"])
+
+        params["encoder"] = torch.empty((n_dict_components, activation_size), device=device)
+        nn.init.orthogonal_(params["encoder"])
+
+        buffers["l1_alpha"] = torch.tensor(l1_alpha, device=device)
+    
+        return params, buffers
+    
+    @staticmethod
+    def loss(params, buffers, batch):
+        c = torch.einsum("nd,bd->bn", params["encoder"], batch)
+        c = F.relu(c)
+
+        normed_weights = nn.functional.normalize(params["decoder"], dim=0)
+
+        x_hat = torch.einsum("nd,bn->bd", normed_weights, c)
+
+        l_reconstruction = F.mse_loss(x_hat, batch)
+        l_l1 = buffers["l1_alpha"] * torch.norm(c, 1, dim=1).mean()
         
         return l_reconstruction + l_l1
--- cluster_runs.py
+++ cluster_runs.py
@@ -6,26 +6,27 @@ import torch.utils.data as data
 
 import torch.multiprocessing as mp
 
-from ensemble import VectorizedEnsemble
+from autoencoders.ensemble import FunctionalEnsemble
 
 # suboptimal solution (have to wait for all jobs to finish using dataloader to exit)
 # works for now
 
-mp.set_start_method("spawn")
-
 # jobs: iterable -> process
 # TODO: queue listener
 def dispatch_on_chunk(ensembles, args, dataset):
-    dataset.share_memory()
+    dataset.share_memory_()
     for ensemble in ensembles:
-        ensemble.share_memory()
+        ensemble.to_shared_memory()
 
     processes = []
     queues = []
-    for i in range(args):
+    for i in range(len(args)):
         q = mp.Queue()
         p = mp.Process(target=train_as_process, args=(i, args[i], q))
-        q.put((ensembles[i].state_dict(), dataset))
+        #q.put((ensembles[i].state_dict(), dataset))
+        ensemble_state = ensembles[i].state_dict()
+        #del ensemble_state["modeldesc"]
+        q.put((ensemble_state, dataset))
 
         p.start()
         processes.append(p)
@@ -39,7 +40,7 @@ def dispatch_on_chunk(ensembles, args, dataset):
 def train_as_process(instance, args, queue):
     state_dict, dataset = queue.get()
 
-    ensemble = VectorizedEnsemble.from_state(state_dict)
+    ensemble = FunctionalEnsemble.from_state(state_dict)
 
     batch_size = args["batch_size"]
     device = args["device"]
@@ -53,16 +54,22 @@ def train_as_process(instance, args, queue):
     )
 
     losses = []
-    for batch_idxs in iter(dataloader):
+    for epoch, batch_idxs in enumerate(iter(sampler)):
+        print(f"Instance {instance} epoch {epoch}")
         batch = dataset[batch_idxs].to(device)
-        loss = ensemble.step_batch(batch)
-        losses.append(loss)
+        ensemble.step_batch(batch)
+        #losses.append(loss.detach().cpu().numpy())
     
     return losses
 
 if __name__ == "__main__":
-    from autoencoders.sae_ensemble import SAE
+    from autoencoders.sae_ensemble import FunctionalSAE
     import torchopt
+
+    torch.set_grad_enabled(False)
+
+    if mp.get_start_method(allow_none=True) != "spawn":
+        mp.set_start_method("spawn")
 
     l1_alphas = [0.001, 0.01, 0.05, 0.1]
     dict_sizes = [1024, 2048, 4096, 8192]
@@ -70,18 +77,24 @@ if __name__ == "__main__":
     activation_size = 1024
 
     ensembles = []
-    for dict_size in dict_sizes:
-        models = [SAE(activation_size, dict_size, l1_alpha) for l1_alpha in l1_alphas]
-        ensemble = VectorizedEnsemble(models, torchopt.Adam(0.001))
+    devices = ["cuda:0", "cuda:1", "cuda:2", "cuda:3"]
+    for i, dict_size in enumerate(dict_sizes):
+        #ensembles.append(DummyEnsemble(devices[i]))
+        models = [FunctionalSAE.init(activation_size, dict_size, l1_alpha) for l1_alpha in l1_alphas]
+        ensemble = FunctionalEnsemble(
+            models, FunctionalSAE.loss,
+            torchopt.adam, {
+                "lr": 0.01
+            },
+            device=devices[i])
         ensembles.append(ensemble)
     
-    devices = ["cuda:0", "cuda:1", "cuda:2", "cuda:3"]
-    args = [{"batch_size": 128, "device": device} for device in devices]
-    for i in range(ensembles):
-        ensemble.to_device(devices[i])
+    target_size = 1048576
+
+    args = [{"batch_size": target_size // dict_sizes[i], "device": devices[i]} for i in range(len(ensembles))]
     
     dataset = torch.randn(100000, activation_size)
 
     outputs = dispatch_on_chunk(ensembles, args, dataset)
-    
+
     print(outputs)
