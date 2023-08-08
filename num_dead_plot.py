feb487cb5d3ad9f9328d04a2165a8f04dd7b6f5c
HoagyC <hoagycunningham@gmail.com>
2023-08-08 16:07:45 -0700

    Small tweaks

--- num_dead_plot.py
+++ num_dead_plot.py
@@ -1,3 +1,5 @@
+from typing import List, Tuple
+
 import standard_metrics
 import matplotlib
 import torch
@@ -25,7 +27,7 @@ if __name__ == "__main__":
             mean_nz = standard_metrics.mean_nonzero_activations(learned_dict, sample)
             sparsity = mean_nz.sum().item()
             #fuv = standard_metrics.fraction_variance_unexplained(learned_dict, sample).item()
-            dead_count = []
+            dead_count: List[Tuple[int, float, float]] = []
             for s in subsample_sizes:
                 num_dead = mean_nz[:s].count_nonzero().item()
                 datapoint_series.append((num_dead, sparsity, hyperparams["l1_alpha"]))
@@ -41,8 +43,8 @@ if __name__ == "__main__":
     fig = plt.figure()
     ax = fig.add_subplot(111)
     for i, datapoint_series in enumerate(datapoints):
-        num_dead, sparsity, s = zip(*datapoint_series)
-        ax.scatter(num_dead, sparsity, c=[math.log10(s) for s in s], label=labels[i], cmap=colors[i], vmin=-5, vmax=-1)
+        num_dead, sparsity, l1s = zip(*datapoint_series)
+        ax.scatter(num_dead, sparsity, c=[math.log10(x) for x in l1s], label=labels[i], cmap=colors[i], vmin=-5, vmax=-1)
     ax.set_xlabel("Sparsity")
     ax.set_ylabel("Num Active Neurons")
     ax.set_xscale("log")
